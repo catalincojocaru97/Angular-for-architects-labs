9f3cec4f2b697de18468f30a20b7e63dcef787c9
Catalin <catalincojocaru97@gmail.com>
2021-11-22 18:05:53 +0200

    lab 2 final

--- rxjs-subjects/src/app/core/subjects.service.ts
+++ rxjs-subjects/src/app/core/subjects.service.ts
@@ -1,24 +1,25 @@
 import { Injectable } from '@angular/core';
-
+import { Subject, Observable, BehaviorSubject, ReplaySubject, AsyncSubject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SubjectsService {
-  
-  //Define Subject and Observable properties here
-
+  private subject$: Subject<string>;
+  observable$: Observable<string>;
 
   constructor() {
     this.init();
   }
 
   init() {
-    // Create Subject and Observable Here 
+    this.subject$ = new BehaviorSubject(null);
+    this.observable$ = this.subject$.asObservable();
 
-
-    // Create interval here
-
+    setInterval(() => {
+      const date = new Date();
+      const time = `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
+      this.subject$.next(time);
+    }, 3000)
   }
-
 }
