2491b1451cee105a32d2e3ebd4a6d212da0db70a
Catalin <catalincojocaru97@gmail.com>
2021-11-22 18:58:14 +0200

    lab4 ngrx-data final

--- state-management/ngrx-data/src/app/app.module.ts
+++ state-management/ngrx-data/src/app/app.module.ts
@@ -7,6 +7,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
 import { AppStoreModule } from './store/app-store.module';
+import { MetaReducer, StoreModule } from '@ngrx/store';
+import { environment } from '../environments/environment';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+
+export const metaReducers: MetaReducer<any>[] = environment.production ? [] : [];
 
 @NgModule({
   imports: [
@@ -15,7 +21,10 @@ import { AppStoreModule } from './store/app-store.module';
     CoreModule,
     HttpClientModule,
     AppRoutingModule,
-    AppStoreModule
+    AppStoreModule,
+    StoreModule.forRoot({}, { metaReducers }),
+    EffectsModule.forRoot([]),
+    !environment.production ? StoreDevtoolsModule.instrument() : []
   ],
   declarations: [AppComponent],
   bootstrap: [AppComponent]
